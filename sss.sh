678013de0abfcf20fb2811dca2dd13d536225cb7
Alireza Amini <alrezamini@yahoo.com>
2022-12-02 14:20:48 +0330

    Bash File Changed

--- sss.sh
+++ sss.sh
@@ -1,10 +1,10 @@
 if [[ `whoami` == "root"  ]];
 then
-	v2l="$1"
 	apt purge shadowsocks-libev && rm -r /etc/shadowsocks-libev && apt-get autoremove
-	apt update && apt install shadowsocks-libev
-	wget -P /etc/shadowsocks-libev/ $v2l
-	tar -xf /etc/shadowsocks-libev/${v2l##*/} -C /etc/shadowsocks-libev/
+	apt update
+	apt install shadowsocks-libev
+	wget -P /etc/shadowsocks-libev/ https://github.com/shadowsocks/v2ray-plugin/releases/download/v1.3.2/v2ray-plugin-linux-amd64-v1.3.2.tar.gz
+	tar -xf /etc/shadowsocks-libev/v2ray-plugin-linux-amd64-v1.3.2.tar.gz -C /etc/shadowsocks-libev/
 	mv /etc/shadowsocks-libev/v2ray-plugin_linux_amd64 /etc/shadowsocks-libev/v2ray-plugin
 	cp /etc/shadowsocks-libev/v2ray-plugin /usr/bin/
 else
